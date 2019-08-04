83dbd85ad96b3fa1be8542be043408ef8b581581
旁观者 <liu123327youyou@163.com>
2019-08-04 19:45:36 +0800

    Update test.vue

--- FirstVue/test.vue
+++ FirstVue/test.vue
@@ -1,7 +1,7 @@
 <template>
 </template>
 
-<script src='https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js'>
+<script src='https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js' type="text/javascript">
 </script>
 
 <style>
